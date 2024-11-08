b77866f520b3bce64588e02ca88c56e90ea801d0
blue <samuelwalter2001@gmail.com>
2024-11-08 17:26:07 -0500

    feat: added protected route component

---
+++ client/src/setup/protected-route.tsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { PropsWithChildren, useEffect } from "react";
+import { useAuth } from "./auth-context";
+
+export const ProtectedRoute = ({ children }: PropsWithChildren) => {
+  const { isAuthenticated, logout } = useAuth();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      logout()
+    }
+  }, [location.pathname])
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children ? <>{children}</> : <Outlet />;
+}
