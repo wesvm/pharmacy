842460ce8be416c920c93b9fff35bc6d391aa3d5
blue <samuelwalter2001@gmail.com>
2024-11-08 15:58:34 -0500

    chore: enable CORS in server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/index.js";
 import errorMiddleware from "./middlewares/error.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.disable("x-powered-by");
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.use(errorMiddleware);
 
