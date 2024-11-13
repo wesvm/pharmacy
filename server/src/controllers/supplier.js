3663af27ae529a991aa9f2ffb12cec900e46450f
blue <samuelwalter2001@gmail.com>
2024-11-13 16:49:04 -0500

    feat: add supplier controller

--- server/src/controllers/supplier.js
+++ server/src/controllers/supplier.js
@@ -5,10 +5,61 @@ class SupplierController {
     const suppliers = await prisma.supplier.findMany();
     res.status(200).json({ suppliers });
   }
-  async getById(req, res, next) {}
-  async create(req, res, next) {}
-  async update(req, res, next) {}
-  async delete(req, res, next) {}
+  async getById(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const supplier = await prisma.supplier.findUnique({
+        where: { id: parseInt(id) }
+      })
+
+      if (!supplier) {
+        return res.status(404).json({ error: "Proveedor no encontrado." });
+      }
+
+      res.status(200).json({ supplier });
+    } catch (error) {
+      next(error)
+    }
+  }
+  async create(req, res, next) {
+    const { name, contactInfo } = req.body
+
+    try {
+      const supplier = await prisma.supplier.create({
+        data: { name, contactInfo },
+      });
+      res.status(201).json({ message: "Proveedor creado correctamente.", supplier });
+    } catch (error) {
+      next(error);
+    }
+
+  }
+  async update(req, res, next) {
+    const { id } = req.params;
+    const { name, contactInfo } = req.body;
+
+    try {
+      const supplier = await prisma.supplier.update({
+        where: { id: parseInt(id) },
+        data: { name, contactInfo },
+      });
+      res.status(200).json({ message: "Proveedor actualizado correctamente.", supplier });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async delete(req, res, next) {
+    const { id } = req.params;
+    try {
+      await prisma.supplier.delete({
+        where: { id: parseInt(id) },
+      });
+      res.status(200).send({ message: "Proveedor eliminado correctamente." });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default new SupplierController();
--- server/src/routes/supplier.js
+++ server/src/routes/supplier.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import supplierController from "../controllers/supplier.js";
+import validation from "../middlewares/validation.js";
+import { createSupplierSchema, updateSupplierSchema } from "../schemas/supplier.js";
 
 const router = Router();
 
 router.get("/", supplierController.getAll);
-router.post("/", supplierController.create);
-router.get("/:id", supplierController.getById);
-router.put("/:id", supplierController.update);
-router.delete("/:id", supplierController.delete);
+router.post("/", validation(createSupplierSchema), supplierController.create);
+router.get("/:id", validation(updateSupplierSchema), supplierController.getById);
+router.put("/:id", validation(updateSupplierSchema), supplierController.update);
+router.delete("/:id", validation(updateSupplierSchema), supplierController.delete);
 
 export default router;
