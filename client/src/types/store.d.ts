af9085b85d5e95b082046dea12f181c0e9d5db26
blue <samuelwalter2001@gmail.com>
2024-11-18 02:02:06 -0500

    feat: add useProductFilters hook for product filters and store for shopping cart management

--- client/src/types/store.d.ts
+++ client/src/types/store.d.ts
@@ -1,14 +1,14 @@
 type Supplier = {
-  id: number,
-  name: string,
-  contactInfo: string | null,
-}
+  id: number;
+  name: string;
+  contactInfo: string | null;
+};
 
 type Category = {
-  id: number,
-  name: string,
-  description: string | null,
-}
+  id: number;
+  name: string;
+  description: string | null;
+};
 
 type Product = {
   id: number;
@@ -20,6 +20,24 @@ type Product = {
   categoryId: number;
   supplierId: number;
   category: string;
+  imageUrl: string | null;
   supplier: string;
   isArchived: boolean;
+};
+
+type ProductFilters = {
+  search?: string;
+  categoryId?: number;
+}
+
+type Sale = {
+  id: number;
+  saleDate: string;
+  total: number;
+  status: 'Completado' | 'Pendiente' | 'Cancelado';
+  customerName: string;
+  customerDNI: string;
+  deliveryId: number;
+  userId: number;
+  user: Omit<User, 'email' | 'role'>;
 };
